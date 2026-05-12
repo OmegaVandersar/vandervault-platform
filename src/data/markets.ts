cde83ea99b95e3a78e8419cc1c425027601b9391
OmegaVandersar <riberabuchi824@gmail.com>
2026-05-12 22:41:24 +0100

    Create markets.ts

---
+++ src/data/markets.ts
@@ -0,0 +1,29 @@
+export const markets = [
+  {
+    symbol: "BTC",
+    name: "Bitcoin",
+    price: "$108,420",
+    change: "+2.84%",
+  },
+
+  {
+    symbol: "ETH",
+    name: "Ethereum",
+    price: "$5,820",
+    change: "+1.26%",
+  },
+
+  {
+    symbol: "SOL",
+    name: "Solana",
+    price: "$245",
+    change: "+6.12%",
+  },
+
+  {
+    symbol: "XRP",
+    name: "Ripple",
+    price: "$3.40",
+    change: "-0.84%",
+  },
+];
