0ac45e5578b6a7a0fad803bb2f9fef8c91f98e55
OmegaVandersar <riberabuchi824@gmail.com>
2026-05-12 22:10:34 +0100

    Create LanguageContext.tsx

---
+++ src/i18n/LanguageContext.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+} from "react";
+
+type Language = "en" | "de" | "fr";
+
+type LanguageContextType = {
+  language: Language;
+  setLanguage: (lang: Language) => void;
+};
+
+const LanguageContext =
+  createContext<LanguageContextType>({
+    language: "en",
+    setLanguage: () => {},
+  });
+
+export function LanguageProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const [language, setLanguage] =
+    useState<Language>("en");
+
+  return (
+    <LanguageContext.Provider
+      value={{
+        language,
+        setLanguage,
+      }}
+    >
+      {children}
+    </LanguageContext.Provider>
+  );
+}
+
+export function useLanguage() {
+  return useContext(LanguageContext);
+}
